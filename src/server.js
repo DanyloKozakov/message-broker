88a99918ec089e0a6be89e479a35d444fd2b9387
Danylo Kozakov <dkozakov@reenbit.com>
2026-07-18 00:24:29 +0200

    Change ip

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ import { handleCompletedRound } from "./handler.js";
 const port = Number(process.env.PORT ?? 3000);
 const server = createApp({ handler: handleCompletedRound });
 
-server.listen(port, () => {
-  console.log(`Worker coordinator listening on http://localhost:${port}`);
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Worker coordinator listening on port ${port}`);
 });
